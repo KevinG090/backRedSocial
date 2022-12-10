3314030941ed7cfc2c288cda5ee74899b51c437f
Your NameYour <kevin.guevara090@gmail.com>
2022-12-10 17:48:42 -0500

    conexion a la base de datos con express, cors y mongodb

---
+++ arrancar-bd.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\6.0\bin && mongod.exe"
+start cmd.exe /k "cd C:\Users\kevin\OneDrive\Documentos\proyectos\Repositorio_Personal\Red_Social_Udemy\api-rest-res-social && npm start"
+@REM start C:\Users\kevin\AppData\Local\MongoDBCompass\MongoDBCompass.exe
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+const  connection  = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
+
 // conexion bd
+console.log("API NODE arrancada")
+connection();
 
 // crear servidor node
+const app = express();
+const puerto = 3900;
 
-// configurar cors
+// configurar cors (se ejecuta antes de las rutas)
+app.use(cors());
 
 // convertir los datos del body a objetos js
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); //cualquier dato codificable lo convierte
 
 // cargar conf rutas
 
+
+// ruta de prueba
+app.get("/ruta-prueba",(req,res)=>{
+    return res.status(200).json({
+        "id":1,
+        "nombre":"Kevin",
+        "web":"kevinGuevara.com"
+    })
+})
+
 // poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("servidor corriendo")
+})
