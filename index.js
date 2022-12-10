7fff7d6a8eac164b16fc2c5731feb49cddf19b88
Your NameYour <kevin.guevara090@gmail.com>
2022-12-10 13:33:28 -0500

    creacion de la carpeta para la api con sus dependencias

---
+++ index.js
@@ -0,0 +1,11 @@
+// conexion bd
+
+// crear servidor node
+
+// configurar cors
+
+// convertir los datos del body a objetos js
+
+// cargar conf rutas
+
+// poner servidor a escuchar peticiones http
